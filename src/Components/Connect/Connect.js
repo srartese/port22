9363503922c151c36e5af9e7d6e030ffb4c4fb18
srartese <saa2457@rit.edu>
2022-11-07 14:32:27 -0800

    fixed spelling

--- src/Components/Connect/Connect.js
+++ src/Components/Connect/Connect.js
@@ -8,7 +8,7 @@ function Connect() {
       <div className="ConnectContents">
       <div className="aboutMe">
       <div className='meContainer'> <img src="images/Media/me.png" className='meImage' alt='Sara standing on beach with arms spread open.'/></div>
-        <div className='infoBlock'> Hey there, my name is Sara! Over the past 4.5 years I have been working full-time for Helios Interactive doing all sorts of client projects but am now looking for a different opportunity to grow in. I am passionate about learning, creating, and solving problems. I can pick up neccessary skills fast with hands on experience. Send me a message with any questions or just to chat! </div>
+        <div className='infoBlock'> Hey there, my name is Sara! Over the past 4.5 years I have been working full-time for Helios Interactive doing all sorts of client projects, but am now looking for a different opportunity to grow in. I am passionate about learning, creating, and solving problems. I can pick up necessary skills fast with hands on experience. Send me a message with any questions or just to chat! </div>
         </div>
         <div className='socialLinks'>
         <a href="https://www.linkedin.com/in/saraartese/" target="_blank">Linkedin</a>
